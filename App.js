f1a862a1cd2d778f148a2edbf6bc13731ec7ea35
rodrigocoe1 <rodrigocoe2@gmail.com>
2022-08-18 14:08:53 -0300

    subindo o site

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React,{ useState } from 'react';
 
 export default function App() {
+  const [nome,setNome] = useState('Light boy');
+  const [sessao, setSessao] = useState('and the Light');
+  const [baixavel,setBaixavel] = useState(true)
+  const clicado = () =>{
+    setNome("Dark boy");
+    setSessao("and the Darkness")
+    setBaixavel(false)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}> { nome } </Text> 
+      <Text style={styles.titulo}> { sessao } </Text>
+      <Text style={styles.titulo}> { baixavel ? "Esta baixavel" :"esta inbaixavel" } </Text>
       <StatusBar style="auto" />
+      <Button title = 'trocar sessao' onPress={clicado}  />
     </View>
   );
 }
@@ -13,8 +27,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    color: '#fff',
+    fontSize: 30,
+  },
+  paragrafo:{
+    fontSize: 15,
+    color: '#fff'
+  }
 });
