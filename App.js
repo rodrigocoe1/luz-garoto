c3f497e78c7d71b44bc30fe47d2ed517185bbc66
rodrigocoe1 <rodrigocoe2@gmail.com>
2022-08-25 15:27:39 -0300

    criando o cripto

--- App.js
+++ App.js
@@ -2,76 +2,24 @@ import {SafeAreaView,FlatList, StyleSheet,Image, Text, View,Button } from 'react
 import React,{useState,useEffect} from 'react';
 import styled from 'styled-components/native';
 export default function App() {
-  const [pokemons,setPokemons] = useState([])
-  useEffect(() =>{
-    fetch('https://pokeapi.co/api/v2/pokemon',{
-        method:'GET',
-        headers: {
-          'Accept': 'application/json',
-        }
-    })
-    .then(response => response.json())
-    .then(data=>{
-      console.log(data)
-      setPokemons(data.results)
-    })
-  },[])
   return (
-    <SafeAreaView style={styles.container}>
-       <FlatList 
-          data={pokemons}
-          keyExtractor={(pokemon) => pokemon.name}
-          contentContainerStyle={{flexGrow: 1}}
-          renderItem={PokemonShow}
-       />
+    <SafeAreaView style= {StyleSheet.container}>
+
     </SafeAreaView>
   );
 }
-function PokemonShow(item) {
-  const {name,url}  = item.item
-  const pokemonNumber = url.replace('https://pokeapi.co/api/v2/pokemon','')
-  const ImageUrl = 'https://cdn.traction.one/pokedex/pokemon'+pokemonNumber+'.png'
-  return(
-    <View style={pokemonEstilo.card}>
-      <NomePokemon styles={styles.titulo}>{name}</NomePokemon >
-      <Image  style={pokemonEstilo.imagem} 
-              source={{uri: ImageUrl.replace('/.png','.png')}}
-      />
 
-    </View>
-  )
-}
-const pokemonEstilo = StyleSheet.create({
-  card: {
-    FlexDirection: 'column',
-    width: '350',
-    height: '350',
-    backgroundColor: '#23412f',
-    margin: 1,
-    justifyContent:'center',
-    alignItems: 'center',
-    borderRadius: 4,
-  },
-  imagem: {
-    width:  300,
-    height: 300,
-  }
-})
-
-const styles = StyleSheet.create({
+const styles =StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#872',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'  
   },
-  titulo:{
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#fff",
+    marginHorizontal: 16,
+    marginTop: 16,
   }
-});
-const NomePokemon = styled.Text`
-  color: #563476;
-  font-size: 30px
-`;
+})
---
+++ components/TabIcon.js
@@ -0,0 +1,15 @@
+!import React from 'react';
+import { View, Text, Image } from 'react-native';
+import {Fonts, Colors} from '..constantes'
+
+const TabIcon =()=>(){
+    return(
+        <View>
+            <Text>
+                MarketoPlace
+            </Text>
+        </View>
+    )
+}
+
+export default TabIcon;
